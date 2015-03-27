d55a64e809f620911080a61c837da8997b10e6cb
Bikash <er.bikash21@gmail.com>
2015-03-27 12:26:08 +0100

    scatter plot and profile plot

--- test.R
+++ test.R
@@ -12,6 +12,39 @@
 #################################################################################################
 #################################################################################################
 library(gmm)
+
+
+makeProfilePlot <- function(mylist,names)
+{
+  require(RColorBrewer)
+  # find out how many variables we want to include
+  numvariables <- length(mylist)
+  # choose 'numvariables' random colours
+  colours <- brewer.pal(numvariables,"Set1")
+  # find out the minimum and maximum values of the variables:
+  mymin <- 1e+20
+  mymax <- 1e-20
+  for (i in 1:numvariables)
+  {
+    vectori <- mylist[[i]]
+    mini <- min(vectori)
+    maxi <- max(vectori)
+    if (mini < mymin) { mymin <- mini }
+    if (maxi > mymax) { mymax <- maxi }
+  }
+  # plot the variables
+  for (i in 1:numvariables)
+  {
+    vectori <- mylist[[i]]
+    namei <- names[i]
+    colouri <- colours[i]
+    if (i == 1) { plot(vectori,col=colouri,type="l",ylim=c(mymin,mymax)) }
+    else         { points(vectori, col=colouri,type="l")                                     }
+    lastxval <- length(vectori)
+    lastyval <- vectori[length(vectori)]
+    text((lastxval-10),(lastyval),namei,col="black",cex=0.6)
+  }
+}
 ### setting path of repo folder.
 getwd()
 setwd("/Users/bikash/repos/google/data/")
@@ -54,9 +87,30 @@ lines(density(Data1$memory_usage),lty=2)
 data.log <- log(Data1)
 # apply PCA - scale. = TRUE is highly 
 # advisable, but default is FALSE. 
-ir.pca <- prcomp(log.ir,
+fit <- prcomp(data.log,
                  center = TRUE,
                  scale. = TRUE) 
+print(fit)
+summary(fit)
+library(devtools)
+install_github("ggbiplot", "vqv")
+
+library(ggbiplot)
+g <- ggbiplot(fit, obs.scale = 1, var.scale = 1, 
+              ellipse = TRUE, 
+              circle = TRUE)
+g <- g + scale_color_discrete(name = '')
+g <- g + theme(legend.direction = 'horizontal', 
+               legend.position = 'top')
+print(g)
+
+library("car")
+scatterplotMatrix(Data1)
+
+library(RColorBrewer)
+names <- c("V1","V2")
+mylist <- list(Data1$cpurate,Data1$memory_usage)
+makeProfilePlot(mylist,names)
 
 
 ### Gaussian Mixture Model (GMM)
