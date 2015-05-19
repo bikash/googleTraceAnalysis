c2ede14c7f7c4f8ebd0315611ea57df8ee13b7a2
Bikash <er.bikash21@gmail.com>
2015-05-19 18:27:36 +0200

    PCA model

--- model.R
+++ model.R
@@ -3,6 +3,8 @@
 getwd()
 setwd("/Users/bikash/repos/google/data/")
 
+source("/Users/bikash/repos/googleTraceAnalysis/tsmeasures.R")
+source("/Users/bikash/repos/googleTraceAnalysis/anomay.R")
 ## reading data from csv file.
 data <- read.csv("task_usage-part-00001-of-00500.csv", header=TRUE)
 
@@ -12,7 +14,59 @@ print("Data Cleaning up process......")
 Data <- data.frame(cpurate=data$X0.03143, memory_usage=data$X0.05389 , page_cache=data$X0.006645 , diskio_time=data$X7.629e.05 , cycle_inst=data$X2.911, 
                    start_date=data$X5612000000)
 
-Data1 <- Data[1:10000,]
+Data1 <- Data[1:30000,]
+Data1 = na.omit(Data1)
+
+x = Data1
+
+anomaly(x)
+# x: a matrix returned by `tsmeasures` function
+nc <- nrow(x)
+x[is.infinite(x)] <- NA # ignore inf values
+naomit.x <- na.omit(x) # ignore missing values
+na.act <- na.action(naomit.x)
+if (is.null(na.act)) {
+  avl <- 1:nc
+} else {
+  avl <- (1:nc)[-na.action(naomit.x)]
+}
+method <- match.arg("hdr")
+# robust PCA space (scaling version)
+  #rbt.pca <- pcaPP::PCAproj(naomit.x, k = 2, center = mean, scale = sd)
+rbt.pca <- princomp(scale(naomit.x, center = TRUE, scale = TRUE),  cor = TRUE)
+scores <- rbt.pca$scores
+scoreswNA <- matrix(, nrow = nc, ncol = 2)
+scoreswNA[avl, ] <- scores
+
+
+
+
+
+
+
+
+
+
+
+## Helps to remove highly correlated dimensional from data
+# we first standardize the data:
+data.scaled <- data.frame( apply(Data1,2,scale) )
+library(corrplot)
+library(caret)
+corMatMy <- cor(data.scaled)
+#compute the correlation matrix
+corrplot(corMatMy, order = "hclust")
+#visualize the matrix, clustering features by correlation index.
+highlyCor <- findCorrelation(corMatMy, 0.70)
+#Apply correlation filter at 0.70,
+#then we remove all the variable correlated with more 0.7.
+datMyFiltered.scale <- data.scaled[,-highlyCor]
+corMatMy <- cor(datMyFiltered.scale)
+corrplot(corMatMy, order = "hclust")
+
+
+##box plot
+boxplot(data.scaled, main="Boxplot of Cluster data trace")
 
 ## Omit NA and data imputation before doing PCA analysis
 #data1 <- data.frame(cpurate=Data1$cpurate, memory_usage=Data1$memory_usage, page_cache = Data1$page_cache, diskio_time=Data1$diskio_time)
@@ -24,6 +78,12 @@ x = c(1:length(y))
 plot(x, y, type="n", xlab="Time", ylab="Value" ) 
 points(y, pch=19, col="red")
 lines(y, pch=19, col="red")
+
+
+## PCA for dmensional reduction
+require(FactoMineR) 
+pca <- PCA(Data1, scale.unit=TRUE, ncp=5, graph=T)
+dimdesc(pca)
 
 
 ## PCA
